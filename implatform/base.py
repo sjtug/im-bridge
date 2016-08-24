df7e8b6ab3a5ef3427abf6d9511c9f56b4b1f217
Vic <lz96@foxmail.com>
2016-08-24 17:55:24 +0800

    Add more doc

--- implatform/base.py
+++ implatform/base.py
@@ -4,16 +4,24 @@ import abc
 class PlatformBase(abc.ABC):
     @abc.abstractmethod
     def __init__(self, manager):
+        """This method will be called to init the platform.
+
+        Don't do complex login here. You may add extra parameter to this, but keep the default one legal."""
         super(PlatformBase, self).__init__()
 
     @abc.abstractmethod
     def init(self):
-        """This method will be called in manager.run"""
+        """This method will be called in manager.run.
+
+        Developers should login here and block until login finishes."""
         pass
 
     @property
     @abc.abstractmethod
     def init_done(self):
+        """Suggests whether init has finished yet.
+
+         Only if all platforms are init_done the mainloop of manager will run."""
         pass
 
     @abc.abstractmethod
